027291021f4ddbd44366ca7770a75f16823a9458
Richard Davis <rich@davisfamily.online>
2025-03-21 13:40:15 +0000

    Skeleton works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,18 +3,14 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
 import { provideStore } from '@ngrx/store';
 import { messageReducer } from './state/message.reducer';
+import { MessageListComponent } from "./message-list/message-list.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MessageListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'message-store-poc-frontend';
 }
-
-bootstrapApplication(AppComponent, { providers: [
-  { provide: RouterOutlet, useValue: RouterOutlet },
-  provideStore({ message: messageReducer })
-] });
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,7 +5,14 @@ import { routes } from './app.routes';
 import { provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { messageReducer } from './state/message.reducer';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideStore(), provideEffects(), provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideStore({ messageState: messageReducer }),
+    provideEffects(),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
+  ]
 };
