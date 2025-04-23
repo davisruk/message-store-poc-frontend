77ea19f3b34992d2e9cd6c87d542533f92764591
Richard Davis <rich@davisfamily.online>
2025-04-23 16:21:33 +0100

    Added close button to message component and wired into store

--- src/app/message/message.component.ts
+++ src/app/message/message.component.ts
@@ -3,12 +3,14 @@ import { Component, inject, Input } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { selectSelectedMessages } from '../state/message.selectors';
 import { Store } from '@ngrx/store';
-import { map, Observable } from 'rxjs';
+import { map, Observable, take } from 'rxjs';
 import { Message } from '../state/state';
+import { MatIconModule } from '@angular/material/icon';
+import { addSelectedMessage } from '../state/message.actions';
 
 @Component({
   selector: 'app-message',
-  imports: [CommonModule, MatCardModule],
+  imports: [CommonModule, MatCardModule, MatIconModule],
   templateUrl: './message.component.html',
   styleUrl: './message.component.css'
 })
@@ -16,8 +18,17 @@ export class MessageComponent {
   store:Store = inject(Store);
   @Input() messageIndex!: number;
   message$: Observable<Message | null>;
+
   constructor(){
     this.message$ = this.store.select(selectSelectedMessages).pipe(
       map(messages => messages[this.messageIndex] ?? null));
+  }
+
+  onClose() {
+    this.message$.pipe(take(1)).subscribe(message => {
+      if (message) {
+        this.store.dispatch(addSelectedMessage({ message }));
+      }
+    });
   }
 }
